5f005b9086e43e867ff76ad6a1626f5bed2eddb8
theooprea <opreatheodor54@gmail.com>
2020-09-06 15:41:19 +0300

    almost done

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.4 (https://startbootstrap.com/themes/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict"; // Start of use strict
+  
+    $('#carousel-id').carousel({ interval: 1000 });
+    $('#carousel-id').carousel('cycle');
+    $('#carouselButton').click(function(){
+        if($("#carouselButton").children('span').hasClass('fa-pause')) {
+            $('#carousel-id').carousel('pause');
+            $("#carouselButton").children('span').removeClass('fa-pause');
+            $("#carouselButton").children('span').addClass('fa-play');
+        }
+        else {
+            $('#carousel-id').carousel('cycle');
+            $("#carouselButton").children('span').removeClass('fa-play');
+            $("#carouselButton").children('span').addClass('fa-pause');
+        }
+    });
+    $("#modal1").click(function() {
+        $("#portfolioModal1").modal('toggle');
+    });
+    $("#modal2").click(function() {
+      $("#portfolioModal2").modal('toggle');
+    });
+    $("#modal3").click(function() {
+      $("#portfolioModal3").modal('toggle');
+    });
+    $("#modal4").click(function() {
+      $("#portfolioModal4").modal('toggle');
+    });
+    $("#modal5").click(function() {
+      $("#portfolioModal5").modal('toggle');
+    });
+    $("#modal6").click(function() {
+      $("#portfolioModal6").modal('toggle');
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+  })(jQuery); // End of use strict
+  
